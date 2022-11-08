8d840786eed06b20f63934b7e9b308bfc84d59b9
Avnish125 <113664320+Avnish125@users.noreply.github.com>
2022-11-08 17:08:01 +0530

    added all scenes
    Added main menu package
    Added deck package in main branch

---
+++ CricX restructured/Assets/inventoryScripts/Card_SO.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+[CreateAssetMenu(fileName = "PlayerCard", menuName = "PlayerCard_SO")]
+
+//Scriptable objects 
+
+public class Card_SO : ScriptableObject,IComparable<Card_SO>
+{   public GameObject cardPrefab;
+    public string cardName;
+    public int stat1;
+    public int stat2;
+    public int stat3;
+    public int stat4;
+    public int stat5;
+    public int stat6;
+
+    public int CompareTo(Card_SO other)
+    {
+        return cardName.CompareTo(other.cardName); 
+    }
+
+}
+
+
