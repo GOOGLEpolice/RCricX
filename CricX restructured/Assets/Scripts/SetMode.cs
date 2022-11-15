86768701068fec3973a6715200a7b45600a4a9ec
GOOGLEpolice <jugalrelan@gmail.com>
2022-11-15 14:28:13 +0530

    bug fixes
    fixed bug on Bowler vs Bowler

--- CricX restructured/Assets/Scripts/SetMode.cs
+++ CricX restructured/Assets/Scripts/SetMode.cs
@@ -11,22 +11,46 @@ public class SetMode : MonoBehaviour
         {
             GameManager.instance.playerCardStats.gameObject.transform.DOShakeRotation(1f,20f,50,80f,false).OnComplete(() =>
             {
-            GameManager.instance.playerCardStats.gameObject.transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => { GameManager.instance.opponentCardSelected = false; Destroy(GameManager.instance.playerCardStats.gameObject); });
+            GameManager.instance.playerCardStats.gameObject.transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => { GameManager.instance.opponentCardSelected = false; Destroy(GameManager.instance.playerCardStats.gameObject); GameManager.instance.playerCardSelected = false; });
             
                 });
             return;
         }
 
+
         if (GameManager.instance.playerRuns < 0 && GameManager.instance.typeOfOpponent == "Batsman")
         {
             GameManager.instance.opponentCardStats.gameObject.transform.DOShakeRotation(1f, 20f, 50, 80f, false).OnComplete(() =>
             {
-                GameManager.instance.opponentCardStats.gameObject.transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => { GameManager.instance.playerCardSelected = false; Destroy(GameManager.instance.opponentCardStats.gameObject); });
+                GameManager.instance.opponentCardStats.gameObject.transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => { GameManager.instance.playerCardSelected = false; Destroy(GameManager.instance.opponentCardStats.gameObject); GameManager.instance.opponentCardSelected = false; });
 
             });
             return;
         }
 
+        
+        if (GameManager.instance.opponentRuns < 0 && GameManager.instance.typeOfPlayer == "Bowler" && GameManager.instance.playerRuns > 0)
+        {
+            GameManager.instance.oppScore = GameManager.instance.oppScore - GameManager.instance.playerRuns;
+            GameManager.instance.playerCardSelected = false;
+            GameManager.instance.opponentCardSelected = false;
+            return;
+        }
+
+        if (GameManager.instance.playerRuns < 0 && GameManager.instance.typeOfOpponent == "Bowler" && GameManager.instance.opponentRuns > 0)
+        {
+            GameManager.instance.mScore = GameManager.instance.mScore - GameManager.instance.opponentRuns;
+            GameManager.instance.playerCardSelected = false;
+            GameManager.instance.opponentCardSelected = false;
+            return;
+        }
+
+        if (GameManager.instance.playerRuns < 0 && GameManager.instance.opponentRuns < 0)
+        {
+            GameManager.instance.playerCardSelected = false;
+            GameManager.instance.opponentCardSelected = false;
+            return;
+        }
        
 
         if (GameManager.instance.playerCardStats.gameObject.tag == "Player" && GameManager.instance.playerCardStats != null)
@@ -39,9 +63,7 @@ public class SetMode : MonoBehaviour
                 {
                     GameManager.instance.mScore = GameManager.instance.mScore + GameManager.instance.playerRuns;
                 }
-            //GameManager.instance.playerCardStats.gameObject.transform.position = GameManager.instance.playerCardStats.startPos;
             GameManager.instance.playerCardSelected = false;
-            
         }
 
             if (GameManager.instance.opponentCardStats.gameObject.tag == "Enemy" && GameManager.instance.opponentCardStats != null)
@@ -55,7 +77,6 @@ public class SetMode : MonoBehaviour
                 {
                     GameManager.instance.oppScore = GameManager.instance.oppScore + GameManager.instance.opponentRuns;
                 }
-            //GameManager.instance.opponentCardStats.gameObject.transform.position = GameManager.instance.opponentCardStats.startPos;
             GameManager.instance.opponentCardSelected = false;
         }
     }
