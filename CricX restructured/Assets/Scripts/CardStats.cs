00416304c7cc3b13ba33038e5f905b7233cfd9a8
GOOGLEpolice <jugalrelan@gmail.com>
2022-11-24 17:03:12 +0530

    To Create Prefabs

--- CricX restructured/Assets/Scripts/CardStats.cs
+++ CricX restructured/Assets/Scripts/CardStats.cs
@@ -9,6 +9,7 @@ public class CardStats : MonoBehaviour
     public PlayerStats playerStats;
     public int playerId;
     public Vector3 startPos;
+    public GameObject playerModel;
     public TMP_Text cardName;
     public TMP_Text ballOne;
     public TMP_Text ballTwo;
@@ -23,6 +24,7 @@ public class CardStats : MonoBehaviour
     {
        
         ShowText();
+       
         
     }
 
@@ -35,9 +37,6 @@ public class CardStats : MonoBehaviour
     void Start()
     {
         playerStats.playerBC = 1;
-        
-        
-        
     }
 
     private void Update()
@@ -99,11 +98,51 @@ public class CardStats : MonoBehaviour
     public void ShowText()
     {
         cardName.text = playerStats.playerName;
-        ballOne.text = playerStats.ball1.ToString();
-        ballTwo.text = playerStats.ball2.ToString();
-        ballThree.text = playerStats.ball3.ToString();
-        ballFour.text = playerStats.ball4.ToString();
-        ballFive.text = playerStats.ball5.ToString();
-        ballSix.text = playerStats.ball6.ToString();
+
+        if (playerStats.ball1 < 0)
+        {
+            ballOne.text = "w".ToString();
+        }
+        else
+            ballOne.text = playerStats.ball1.ToString();
+
+        if (playerStats.ball2 < 0)
+        {
+            ballTwo.text = "w".ToString();
+        }
+        else
+            ballTwo.text = playerStats.ball2.ToString();
+
+        if (playerStats.ball3 < 0)
+        {
+            ballThree.text = "w".ToString();
+        }
+        else
+            ballThree.text = playerStats.ball3.ToString();
+
+        if (playerStats.ball4 < 0)
+        {
+            ballFour.text = "w".ToString();
+        }
+        else
+            ballFour.text = playerStats.ball4.ToString();
+
+        if (playerStats.ball5 < 0)
+        {
+            ballFive.text = "w".ToString();
+        }
+        else
+            ballFive.text = playerStats.ball5.ToString();
+
+        if (playerStats.ball6 < 0)
+        {
+            ballSix.text = "w".ToString();
+        }
+        else
+            ballSix.text = playerStats.ball6.ToString();
+            
+        
+        
+        
     }
 }
